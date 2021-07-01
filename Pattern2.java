8a575a3bf359cfcb852bf44a24afba91da4a846a
saisri-02 <71486510+saisri-02@users.noreply.github.com>
2021-07-01 22:20:24 +0530

    initial commit

---
+++ Pattern2.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Pattern2
+{
+
+public static void main (String[] args) 
+{
+
+int n,i,j;
+  
+Scanner sc=new Scanner(System.in);
+  
+System.out.println("Enter n value : ");
+  
+n=sc.nextInt();
+  
+for(i=1;i<=n;i++)
+  
+{
+      
+for(j=1;j<=i;j++)
+      
+{
+          
+System.out.println("*");
+      
+}
+      
+System.out.println( );
+  
+}
+  
+for(i=n-1;i>=1;i--)
+  
+{
+      
+for(j=1;j<=i;j++)
+      
+{
+          
+System.out.println("*");
+      
+}
+      
+System.out.println( );
+  
+}
+
+}
+
+}
